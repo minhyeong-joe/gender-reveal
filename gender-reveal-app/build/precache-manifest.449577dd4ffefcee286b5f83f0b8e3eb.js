61ccf718408e2972f0d4852d805db2eb9388cf56
minhyeong-joe <minhyeong.billy.joe@gmail.com>
2024-10-26 00:13:48 -0700

    final

---
+++ gender-reveal-app/build/precache-manifest.449577dd4ffefcee286b5f83f0b8e3eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cefe0d5e5e82597d2de463eb891780f6",
+    "url": "/gender-reveal/index.html"
+  },
+  {
+    "revision": "1ba82e3440545cd9f3ed",
+    "url": "/gender-reveal/static/css/main.dc862cc5.chunk.css"
+  },
+  {
+    "revision": "585e1266a33b2a3fc0e7",
+    "url": "/gender-reveal/static/js/2.9dd64206.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/gender-reveal/static/js/2.9dd64206.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afabb76aa91df2333cfb",
+    "url": "/gender-reveal/static/js/3.5d2dbc52.chunk.js"
+  },
+  {
+    "revision": "1ba82e3440545cd9f3ed",
+    "url": "/gender-reveal/static/js/main.d4451da4.chunk.js"
+  },
+  {
+    "revision": "d66e99878b68f000a370",
+    "url": "/gender-reveal/static/js/runtime-main.4250c06f.js"
+  }
+]);
--- gender-reveal-app/src/App.js
+++ gender-reveal-app/src/App.js
@@ -1,23 +1,97 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import Confetti from "react-confetti"; // Import the Confetti component
+import "./App.css"; // Make sure you have the styles in App.css
+
+// Balloon sound file path
+const popSound = new Audio(process.env.PUBLIC_URL + "/pop.mp3");
+
+// Function to shuffle the array
+const shuffleArray = (array) => {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[array[i], array[j]] = [array[j], array[i]];
+	}
+	return array;
+};
 
 function App() {
+	const initialBalloons = [
+		{ id: 1, isBoy: false, popped: false },
+		{ id: 2, isBoy: false, popped: false },
+		{ id: 3, isBoy: false, popped: false },
+		{ id: 4, isBoy: true, popped: false }, // This balloon reveals the boy gender
+	];
+
+	const [balloons, setBalloons] = useState(shuffleArray(initialBalloons)); // Shuffle on initialization
+	const [revealed, setRevealed] = useState(false);
+	const [gender, setGender] = useState(null);
+	const [isClickable, setIsClickable] = useState(true); // State to track if balloons are clickable
+
+	const handlePop = (isBoy, id) => {
+		// Play sound
+		popSound.play();
+
+		// Update balloon state to mark as popped
+		const updatedBalloons = balloons.map((balloon) =>
+			balloon.id === id ? { ...balloon, popped: true } : balloon
+		);
+
+		setBalloons(updatedBalloons); // Update the balloons state
+		setRevealed(true);
+		setGender(isBoy ? "Boy" : "Try again!");
+
+		// Disable clicking if the popped balloon reveals the boy gender
+		if (isBoy) {
+			setIsClickable(false); // Disable further clicks on remaining balloons
+		}
+	};
+
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
+		<div className="app">
+			<header className="app-header">
+				<h1>Gender Reveal Game</h1>
+				<p>Pop a balloon to find out!</p>
+				<div className="balloons-container">
+					{balloons.map(
+						(balloon) =>
+							!balloon.popped && ( // Only show balloons that haven't been popped
+								<div
+									key={balloon.id}
+									className={`balloon ${balloon.popped ? "popped" : ""}`}
+									onClick={
+										isClickable
+											? () => handlePop(balloon.isBoy, balloon.id)
+											: null
+									} // Disable clicking if not clickable
+								>
+									🎈
+								</div>
+							)
+					)}
+				</div>
+				{revealed && (
+					<div className="reveal-message">
+						{gender === "Boy" && (
+							<>
+								<Confetti
+									width={window.innerWidth}
+									height={window.innerHeight}
+									numberOfPieces={800} // Increase density
+									recycle={false} // Prevent recycling for a one-time effect
+									gravity={0.2} // Faster falling confetti
+									initialVelocityY={10} // Shoot confetti up faster
+								/>{" "}
+								{/* Add confetti effect */}
+								<img
+									src={`${process.env.PUBLIC_URL}/its-a-boy.png`}
+									alt="It's a boy!"
+									className="gender-image"
+								/>
+							</>
+						)}
+						{gender === "Try again!" && <p>Pick another balloon!</p>}
+					</div>
+				)}
 			</header>
 		</div>
 	);
