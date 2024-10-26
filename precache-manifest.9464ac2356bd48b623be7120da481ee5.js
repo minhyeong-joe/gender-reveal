c853a86cc34a376ec6485d25e54b5254d39688d8
minhyeong-joe <minhyeong.billy.joe@gmail.com>
2024-10-25 23:28:03 -0700

    Updates

---
+++ precache-manifest.9464ac2356bd48b623be7120da481ee5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b760a0e78d50e960325c7e1b23d6276a",
+    "url": "/gender-reveal/index.html"
+  },
+  {
+    "revision": "435a2e164f43f288cca0",
+    "url": "/gender-reveal/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "e06902b1b949451aa7e4",
+    "url": "/gender-reveal/static/js/2.1f4a886f.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/gender-reveal/static/js/2.1f4a886f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afabb76aa91df2333cfb",
+    "url": "/gender-reveal/static/js/3.5d2dbc52.chunk.js"
+  },
+  {
+    "revision": "435a2e164f43f288cca0",
+    "url": "/gender-reveal/static/js/main.bbc2009b.chunk.js"
+  },
+  {
+    "revision": "d66e99878b68f000a370",
+    "url": "/gender-reveal/static/js/runtime-main.4250c06f.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/gender-reveal/static/media/logo.06e73328.svg"
+  }
+]);
